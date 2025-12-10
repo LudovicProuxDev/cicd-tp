6350e9a710455ae55ca9714769aeef94d4323cd6
LudovicProuxDev <LudovicProuxDev@users.noreply.github.com>
2025-12-10 15:06:34 +0000

    deploy: 40081fba1b1d913a911a1ef4a1980f728351badd

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  reporters: [
-    "default",
-    ["jest-allure2-reporter", {
-      resultsDir: "allure-results"
-    }]
-  ]
-};
--- tests/unit/greeting.test.js
+++
@@ -1,7 +0,0 @@
-const { getGreeting } = require("../../src/greeting");
-
-describe("getGreeting", () => {
-  it("returns the hello world message", () => {
-    expect(getGreeting()).toBe("Hello world!");
-  });
-});
